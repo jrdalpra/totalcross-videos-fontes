735f90b3c8d201d5f1ad304a62e3af72c5c0278a
jrdalpra <jrdalpra@4bbcbaa0-1be1-11df-91d3-e12c3dea33da>
2010-02-17 19:07:13 +0000

    Initial import

---
+++ OlaMundo/src/Main.java
@@ -0,0 +1,12 @@
+import totalcross.ui.Label;
+import totalcross.ui.MainWindow;
+
+public class Main extends MainWindow {
+
+	public void initUI() {
+
+		add(new Label("Ola Mundo com TotalCross!"), CENTER, CENTER);
+
+	}
+
+}
